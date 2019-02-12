a3adb629bf78b0076d6b4cb81b2bfa81621bf642
Sarah <sjmadro1@gmail.com>
2019-02-12 21:39:48 +0000

    added urls_show

--- express_server.js
+++ express_server.js
@@ -30,3 +30,11 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
   console.log('*****', templateVars);
 });
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  };
+  res.render("urls_show", templateVars);
+});
