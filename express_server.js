80f9c0c9c3a7ef8bce2794f65f006336ffd3f270
Sarah <sjmadro1@gmail.com>
2019-02-13 22:04:25 +0000

    delete rout working

--- express_server.js
+++ express_server.js
@@ -71,3 +71,10 @@ app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
+
+app.post('/urls/:shortURL/delete', (req, res) => {
+  let shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  console.log('****', shortURL);
+  res.redirect('/urls');
+});
