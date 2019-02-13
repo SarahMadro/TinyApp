8b7432e484e5488b58af4266d31e7003b2f1eb2d
Sarah <sjmadro1@gmail.com>
2019-02-13 01:15:45 +0000

    created longURL form that sends data via post, created route that handles post requests and added funciton that generates random string

--- express_server.js
+++ express_server.js
@@ -2,7 +2,17 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+  let r = Math.random().toString(36).substring(7);
+  console.log(r);
+};
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -25,10 +35,9 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get('/urls', (req, res) => {
-  let templateVars = { urls: urlDatabase};
-  res.render('urls_index', templateVars);
-  console.log('*****', templateVars);
+
+app.get("/urls/new", (req, res) => { //renders urls_show
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -37,4 +46,15 @@ app.get("/urls/:shortURL", (req, res) => {
     longURL: urlDatabase[req.params.shortURL]
   };
   res.render("urls_show", templateVars);
+});
+
+app.get('/urls', (req, res) => {
+  let templateVars = { urls: urlDatabase};
+  res.render('urls_index', templateVars);
+  console.log('*****', templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
