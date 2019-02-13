9ad663324ed620edc81ea66abbafb4b4b698d297
Sarah <sjmadro1@gmail.com>
2019-02-13 23:35:16 +0000

    updates url resource

--- express_server.js
+++ express_server.js
@@ -42,7 +42,6 @@ app.get("/urls/new", (req, res) => { //renders urls_show
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  console.log('******', req.params);
   let templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
@@ -58,23 +57,23 @@ app.get('/urls', (req, res) => {
 app.post("/urls", (req, res) => {
   let randomStr = generateRandomString(7);
   urlDatabase[randomStr] = req.body.longURL;
-  console.log(req.body);
-  // let ht = 'http';
-  // if(longURL.includes(`${ht}`) {
-
-  // }
   res.redirect(`/urls/${randomStr}`);
 
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  let longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
 app.post('/urls/:shortURL/delete', (req, res) => {
   let shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
-  console.log('****', shortURL);
   res.redirect('/urls');
+});
+
+app.post('/urls/:shortURL', (req, res) => {
+  let shortURL = req.params.shortURL;
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect("/urls/");
 });
